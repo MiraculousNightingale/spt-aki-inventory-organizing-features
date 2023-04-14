a346eaf4236b2fb2fce7e22b957242582cf6a0e3
Nightingale <esteticwence@gmail.com>
2023-04-14 15:23:24 +0300

    first commit

---
+++ client-side/InventoryOrganizingFeatures/obj/Debug/net472/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace InventoryOrganizingFeatures
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "InventoryOrganizingFeatures";
+        public const string PLUGIN_NAME = "InventoryOrganizingFeatures";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
