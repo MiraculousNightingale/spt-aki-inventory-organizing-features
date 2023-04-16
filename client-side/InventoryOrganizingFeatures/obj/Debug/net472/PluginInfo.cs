21d9b148b975521a097d948b447854a0d57098f5
Nightingale <esteticwence@gmail.com>
2023-04-16 23:16:44 +0300

    Untrack ignored files

--- client-side/InventoryOrganizingFeatures/obj/Debug/net472/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace InventoryOrganizingFeatures
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "InventoryOrganizingFeatures";
-        public const string PLUGIN_NAME = "InventoryOrganizingFeatures";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
